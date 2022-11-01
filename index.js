cade2731eeb2f561f89c04a9854bf4fe8d6456a5
Ahmad Shah <ah.seerat@gmail.com>
2022-11-01 11:21:38 -0400

    commit tree 507973d62645990cb4c82b5c78b9a769af5f3f88

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-/* Given Code, don't edit */
-
-document.querySelector("button").addEventListener("click", handleClick);
-
-function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+document.querySelector("button").addEventListener("click", handleClickk);
+function handleClickk(e) {
+  const timeStringg = document.getElementById('time').value
+  displayMessage(greet(timeStringg)) // messages, greet 
 }
-
-/* End Given Code, don't edit above here...*/
-
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg; //update the content
+}
+function greet(time24) {
+  const hour = parseInt(time24, 10);
+  if ( hour < 12) return "Good Morning" // msg
+  if ( hour > 17) return "Good Evening" // msg
+  return "Good Afternoon" //msg
+}
